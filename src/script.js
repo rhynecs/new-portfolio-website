7652a31aed65c968e3cb0d6ae0b4b5d3ceb89bc7
rhynecs <contact@henrystevens.me>
2022-06-12 14:12:00 +1000

    first page responsive design done, second mobile design mostly complete

--- src/script.js
+++ src/script.js
@@ -1,14 +1,9 @@
-
 import LocomotiveScroll from 'locomotive-scroll'
-
-const backToTop = document.querySelector(".back-to-top");
-const opacityClass = "opacity-0";
-const visibilityClass = "invisible";
 
 const scroll = new LocomotiveScroll({
 	el: document.querySelector("[data-scroll-container]"),
-	smooth: true,
-	tablet: { smooth: true },
-	smartphone: { smooth: true },
+	smooth: false,
+	// tablet: { smooth: true },
+	// smartphone: { smooth: true },
 });
 
